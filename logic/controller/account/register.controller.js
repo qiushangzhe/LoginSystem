8343a4518b3b5018d7ddb0dc0a7c5af3aad20734
qiushangzhe <qiusz@jiqdaibao.com>
2017-05-02 18:38:46 +0800

    注册路由返回值修改

--- logic/controller/account/register.controller.js
+++ logic/controller/account/register.controller.js
@@ -2,14 +2,15 @@
     账号注册接口
 */
 let userDb = require('../../../database/account/register.db.js');
+let message = require('../../common/msg_creater.js');
 module.exports = function(req,res){
     var data = req.body;
     console.log('服务器收到消息:',data);
     userDb.registerUser(data).then(function(data){
-        res.send('注册成功');
+        res.send(message.success_msg('注册成功',{}));
         res.end();
     },function(err){
-        res.send('注册失败');
+        res.send(message.error_msg(20000,'注册失败'));
         res.end();
     });
 }
